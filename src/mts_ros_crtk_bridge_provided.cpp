c49b2d672e8130eb603fea2ea9e4c3e5590b83c6
Anton Deguet <anton.deguet@jhu.edu>
2022-08-14 14:42:11 -0400

    added support for `servo_ci` (was known as `set_cartesian_impedance_gains` on dVRK)

--- src/mts_ros_crtk_bridge_provided.cpp
+++ src/mts_ros_crtk_bridge_provided.cpp
@@ -349,6 +349,10 @@ void mts_ros_crtk_bridge_provided::bridge_interface_provided(const std::string &
                 m_subscribers_bridge->AddSubscriberToCommandWrite<bool,
                                                                   std_msgs::msg::Bool>
                     (_required_interface_name, _command, _ros_topic);
+            } else if (_crtk_command == "servo_ci") {
+                m_subscribers_bridge->AddSubscriberToCommandWrite<prmCartesianImpedanceGains,
+                                                                  cisst_msgs::msg::CartesianImpedanceGains>
+                    (_required_interface_name, _command, _ros_topic);
             }
         }
     }
